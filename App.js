e0ea295032bb01948c802fcf570c6f0847bbf1dc
Ayush kumar <ayushverma8900@gmail.com>
2024-04-11 11:24:00 +0530

    map added

--- App.js
+++ App.js
@@ -1,17 +1,56 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MapComponent from './Map';
 import React, { useState, useEffect } from 'react';
 import { Button} from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { initializeApp } from 'firebase/app';
+import { getFirestore,setDoc,doc } from 'firebase/firestore';
+import { getDatabase, ref, push, onValue, remove } from "firebase/database";
+import firebase from 'firebase/app';
 
 const Stack = createStackNavigator(); 
 export default function App() {
+   const[data,setData] = useState([]);
+  const firebaseConfig = {
+    apiKey: "AIzaSyB0pJMZ5BOqHKM9V6jswqqawwga4OQPV28",
+    authDomain: "fir-dc608.firebaseapp.com",
+    databaseURL: "https://fir-dc608-default-rtdb.firebaseio.com",
+    projectId: "fir-dc608",
+    storageBucket: "fir-dc608.appspot.com",
+    messagingSenderId: "362096921282",
+    appId: "1:362096921282:web:4fe7d6cbd889f3c226895b"
+  }
+
+  const location_database = initializeApp(firebaseConfig);
+  
+  const dataBase = getDatabase(location_database);
+  const location = ref(dataBase, "Location");
+  console.log(location);
+
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
   const [region, setRegion] = useState({
-    latitude: 22.2535221,
+    latitude: 22.2535222,
     longitude: 84.9083016,
   });
+  
+  useEffect(() => {
+    const locationRef = ref(getDatabase(location_database), 'Location');
+
+    onValue(locationRef, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+            setLatitude(data.latitude);
+            setLongitude(data.longitude);
+            setRegion({latitude,longitude})
+        }
+    });
+}, [latitude]);
+console.log(latitude);
+
 
   const [errorMsg, setErrorMsg] = useState(null);
 
@@ -42,9 +81,9 @@ export default function App() {
     );
   };
 
-  useEffect(() => {
-    // You may initialize the map here if needed
-  }, []);
+  // useEffect(() => {
+  //   // You may initialize the map here if needed
+  // }, []);
 
   return (
     <View style={styles.container}>
@@ -53,9 +92,7 @@ export default function App() {
       </MapView>
 
       <View style={styles.buttonContainer}>
-      </View>
-
-      {errorMsg && <Text style={styles.errorText}>{errorMsg}</Text>}
+      </View> 
     </View>
   );
 }
---
+++ Begin.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push, onValue, remove } from "firebase/database";
+// import { Link } from "react-router-dom";
+// import "./App.css";
+function Begin() {
+  const [message, setMessage] = useState("");
+  const [data, setData] = useState([]);
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyB0pJMZ5BOqHKM9V6jswqqawwga4OQPV28",
+    authDomain: "fir-dc608.firebaseapp.com",
+    databaseURL: "https://fir-dc608-default-rtdb.firebaseio.com",
+    projectId: "fir-dc608",
+    storageBucket: "fir-dc608.appspot.com",
+    messagingSenderId: "362096921282",
+    appId: "1:362096921282:web:4fe7d6cbd889f3c226895b",
+  };
+  const app = initializeApp(firebaseConfig);
+  const dataBase = getDatabase(app);
+  const movies = ref(dataBase, "movies");
+  const handleClick = () => {
+    // console.log(message);
+    push(todoInDb, message);
+    delmessage();
+  };
+  const delmessage = () => {
+    setMessage("");
+  };
+  useEffect(() => {
+    retrieve();
+  }, []);
+  const retrieve = () => {
+    onValue(todoInDb, (snapshot) => {
+      let arrayList = [];
+      if (snapshot.val()) {
+        arrayList = Object.entries(snapshot.val());
+      }
+      // console.log(arrayList);
+      setData(arrayList);
+    });
+  };
+  const removeData = (id) => {
+    let exactLocation = ref(dataBase, `toDOlIST/${id}`);
+    console.log(exactLocation);
+    remove(exactLocation);
+  };
+  return (
+    <div className="Appp">
+      <main className="Begin">
+        <input
+          type="text"
+          onChange={handleChange}
+          className="input"
+          value={message}
+          placeholder="input daily tasks"
+        />
+        <div className="btn">
+          <button onClick={handleClick}>Add</button>
+          <button onClick={retrieve}>reterireve</button>
+        </div>
+      </main>
+      {data ? (
+        <div>
+          {data.map((employee) => {
+            return (
+              <div>
+                <button
+                  style={{ border: "none", backgroundColor: "white" }}
+                  onClick={() => removeData(employee[0])}
+                >
+                  <p>{employee[1]}</p>
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        ""
+      )}
+      <a href="/image">Upload Image</a>
+    </div>
+  );
+}
+
+export default Begin;
